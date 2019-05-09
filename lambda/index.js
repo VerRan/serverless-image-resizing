f8e91affbf2e0f1d520b1bb2c8ae4e24b3339d30
刘恒涛 <liu_ht@126.com>
2019-05-09 18:24:28 +0800

    add support fomate http://url/blue_marble.jpg?size=300x300

--- lambda/index.js
+++ lambda/index.js
@@ -16,15 +16,23 @@ if (process.env.ALLOWED_DIMENSIONS) {
 }
 
 exports.handler = function(event, context, callback) {
-  const key = event.queryStringParameters.key;
-  console.log(key);
-  const match = key.match(/((\d+)x(\d+))\/(.*)/);
-  console.log(match)
-  const dimensions = match[1];
-  const width = parseInt(match[2], 10);
-  console.log(width)
-  const height = parseInt(match[3], 10);
-  const originalKey = match[4];
+  const key = event.queryStringParameters.key;  
+  /**support ulr fomate http://lht-image-resize.s3-website-ap-southeast-1.amazonaws.com/300x300/blue_marble.jpg **/
+//   const match = key.match(/((\d+)x(\d+))\/(.*)/);
+//   const dimensions = match[1];
+//   const width = parseInt(match[2], 10);
+//   const height = parseInt(match[3], 10);
+//   const originalKey = match[4];
+  
+  /***support formate http://lht-image-resize.s3-website-ap-southeast-1.amazonaws.com/blue_marble.jpg?size=300x300**/
+  const match_fist = key.match(/[^\/]+$/);
+  var filename_param = match_fist[0];//parse filename and param string
+  const originalKey=filename_param.split('?')[0];//get fileName for orginalKey
+  const match = filename_param.match(/(\d+)x(\d+)/);
+  const dimensions = match[0] 
+  const width = parseInt(match[1], 10); 
+  const height = parseInt(match[2], 10); 
+  
 
   if(ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) {
      callback(null, {
